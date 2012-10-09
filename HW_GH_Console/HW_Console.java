d2a23bf03b4ff37887985ba4d6fd79c29decd7f5
berthoud <berthoud@yerkes.uchicago.edu>
2012-10-08 20:02:43 -0500

    Updated Comment

---
+++ HW_GH_Console/HW_Console.java
@@ -0,0 +1,11 @@
+public class HW_Console{
+
+    /**
+     * Program for Hello World - GitHub - Console
+     * @param args
+     */
+    public static void main(String[] args) {
+    	System.out.println("Hello World Console!");
+    }
+
+}
